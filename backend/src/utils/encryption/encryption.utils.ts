9cb007ec872fe184e18f4b7ed4e5b0f1bc7f44f0
Fatih BARAÇKILIÇ <fatihb469@gmail.com>
2024-05-11 10:23:31 +0300

    Encryption class generated

---
+++ backend/src/utils/encryption/encryption.utils.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@nestjs/common";
+import * as bcrypt from "bcrypt";
+
+@Injectable()
+export class EncryptionService {
+  public async encrypt(data: string): Promise<string> {
+    const salt = await bcrypt.genSalt();
+    const hash = await bcrypt.hash(data, salt);
+    return hash;
+  }
+
+  public async compare(data: string, encryptedData: string): Promise<boolean> {
+    return await bcrypt.compare(data, encryptedData);
+  }
+}
