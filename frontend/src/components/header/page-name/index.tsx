533a6a2e6072f0601826cb8955f1f054c8581468
Fatih BARAÇKILIÇ <fatihb469@gmail.com>
2024-06-06 23:56:31 +0300

    sidebar and header bugs fixed

--- frontend/src/components/header/page-name/index.tsx
+++ frontend/src/components/header/page-name/index.tsx
@@ -1,10 +1,26 @@
 "use client";
 
+import { navigationItems } from "@/lib/constant";
 import { usePageStore } from "@/stores";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 type Props = {};
 
 export function PageName({}: Props) {
-  const pageName = usePageStore((state) => state.pageName);
+  const { pageName, href, setPage } = usePageStore((state) => state);
+  const path = usePathname();
+
+  useEffect(() => {
+    if (path !== href) {
+      const page = navigationItems.find((item) =>
+        item.href === "/" ? path === "/" : path.startsWith(item.href),
+      );
+      if (page) {
+        setPage(page.title, page.href);
+      }
+    }
+  }, [path, href, setPage, pageName]);
+
   return <h2 className="text-xl">{pageName}</h2>;
 }
