09219789e43b31d03006846e3b829302cdce3d14
Fatih BARAÇKILIÇ <fatihb469@gmail.com>
2024-06-06 23:59:04 +0300

    refactor: Remove unnecessary padding from sidebar component

--- frontend/src/components/sidebar/index.tsx
+++ frontend/src/components/sidebar/index.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 export function Sidebar({}: Props) {
   return (
-    <aside className="hidden w-48 min-w-48 max-w-48 border-r bg-background md:block lg:w-64 lg:min-w-64 lg:max-w-64 lg:px-4">
+    <aside className="hidden w-48 min-w-48 max-w-48 border-r bg-background md:block lg:w-64 lg:min-w-64 lg:max-w-64">
       <nav className="flex flex-col gap-2 p-4">
         {navigationItems.map((item, index) => (
           <SidebarItem key={index} item={item} />
