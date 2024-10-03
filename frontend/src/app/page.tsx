32b42a141e04d3bcc59507dfbca5d098f6d9edc1
Fatih BARAÇKILIÇ <fatihb469@gmail.com>
2024-10-03 19:33:12 +0300

    Refactor: Update homepage to display the app name
    - Import the APP_NAME constant from app.constant.ts
    - Update the return statement to display the app name in a heading element
    - Improve the styling of the heading element
    
    Closes #123

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { APP_NAME } from "@/lib/constant/app.constant";
 import type { Metadata } from "next";
 
 type Props = {};
@@ -7,5 +8,11 @@ export const metadata: Metadata = {
 };
 
 export default async function HomePage({}: Props) {
-  return <>App Page</>;
+  return (
+    <main className="grid min-h-screen w-full place-items-center">
+      <h1 className="text-center text-xl sm:text-3xl md:text-5xl lg:text-7xl xl:text-9xl">
+        {APP_NAME}
+      </h1>
+    </main>
+  );
 }
---
+++ frontend/src/lib/constant/app.constant.ts
@@ -0,0 +1,1 @@
+export const APP_NAME = "Router OS Network";
