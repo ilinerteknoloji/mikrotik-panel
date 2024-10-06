207987c84765bd718b4b6160771682f84c81541d
Fatih BARAÇKILIÇ <fatihb469@gmail.com>
2024-10-06 14:35:12 +0300

    App name changed

--- frontend/src/components/general/header/index.tsx
+++ frontend/src/components/general/header/index.tsx
@@ -1,6 +1,7 @@
 import { authConfig } from "@/app/api/(auth)/auth/[...nextauth]/auth.config";
 import { Button } from "@/components/ui/button";
 import Logo from "@/images/logo.svg";
+import { APP_NAME } from "@/lib/constant/app.constant";
 import { Bell, Settings } from "lucide-react";
 import { getServerSession } from "next-auth";
 import Image from "next/image";
@@ -10,7 +11,6 @@ import { PageName } from "./page-name";
 import { ProfileButton } from "./profile-button";
 import { SearchInput } from "./search-input";
 import { ThemeButton } from "./theme-button";
-import { APP_NAME, APP_NAME_SHORT } from "@/lib/constant/app.constant";
 
 type Props = {
   type: "admin" | "dashboard";
@@ -27,9 +27,7 @@ export async function Header({ type }: Props) {
           className="hidden h-full w-48 min-w-48 max-w-48 items-center gap-2 border-r px-2 py-2 md:flex lg:w-64 lg:min-w-64 lg:max-w-64 lg:px-4"
         >
           <Image src={Logo} alt={`${APP_NAME} Logo`} className="size-16" />
-          <h1 className="text-2xl font-semibold text-white">
-            {APP_NAME_SHORT}
-          </h1>
+          <h1 className="text-2xl font-semibold text-white">{APP_NAME}</h1>
         </Link>
 
         <div className="flex w-full items-center justify-between gap-4 px-4">
--- frontend/src/lib/constant/app.constant.ts
+++ frontend/src/lib/constant/app.constant.ts
@@ -1,2 +1,1 @@
-export const APP_NAME = "Router OS Network";
-export const APP_NAME_SHORT = "Router OS";
+export const APP_NAME = "RouterOS";
